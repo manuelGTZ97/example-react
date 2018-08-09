ed7ba6e788b13374267dfa9b97f3064ce6249088
Manuel Gutierrez <manu.gtzp@gmail.com>
2018-08-09 17:51:24 -0500

    eh

--- src/Layouts/Detail.js
+++ src/Layouts/Detail.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Models from '../CssComponents/Models';
 
 export default class Detail extends Component {
     render(){
         return(
-            <div> 
-                Realizar la lista de modelos
+            <div style={margin}>
+                <Models />
             </div>
         );
     }
 }
+
+const margin ={
+    'margin-top' : '20px'
+};
