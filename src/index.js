2986764048ba5b09be882c0d0988d00ee3423a7d
Manuel Gutierrez <37588547+manuelGTZ97@users.noreply.github.com>
2018-10-30 16:05:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+/*var state = {
+    isEnglish: false
+};
+
+var i = Object.assign({}, state, {isEnglish: true});
+console.log(i);
+
+//Only for iterables (Array not objects)
+var a = [1,2,3,4,5];
+var b = [6,7,8,9,10];
+
+console.log(...a,...b);*/
